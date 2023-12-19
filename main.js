a0ff6d661a183ff473d6ae3622bc814bc3a826fd
abidinalrabi <abidinalrabi2223@gmail.com>
2023-12-19 17:48:07 +0700

    penambahan alert

---
+++ main.js
@@ -0,0 +1,3 @@
+let kataPertama = prompt("Please insert word basamalah");
+alert("Semoga keberkahan selalu meliputi kita dengan pembacaan  " + kataPertama);
+alert("Selamat datang di website kami. jika membutuhkan layanan kami segera hubungi kami ");
