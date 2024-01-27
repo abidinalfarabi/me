c63bf162de2ad9a8976c1d48b25e424a1de7a2a7
abidinalrabi <abidinalrabi2223@gmail.com>
2024-01-27 10:03:06 +0700

    Main js dihapus

--- main.js
+++
@@ -1,3 +0,0 @@
-let kataPertama = prompt("Please insert word basamalah");
-alert("Semoga keberkahan selalu meliputi kita dengan pembacaan  " + kataPertama);
-alert("Selamat datang di website kami. jika membutuhkan layanan kami segera hubungi kami ");
